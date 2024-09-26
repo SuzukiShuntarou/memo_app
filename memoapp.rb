b0bb815a397fba52b028e8d7332dead37bc5df45
SuzukiShuntarou <da8801593@gmail.com>
2024-09-26 21:28:06 +0900

    Delete before action & logical deletion and Change storage in JSON database & escape process

--- memoapp.rb
+++ memoapp.rb
@@ -4,92 +4,77 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 
-MEMOS_FILE = 'public/memos.json'
-
-before do
-  @memo = JSON.parse(File.read(MEMOS_FILE))
-end
-
 get '/memos' do
+  load
   @page_title = 'Top'
-  erb :home, layout: :layout
+  erb :home
 end
 
 get '/memos/new' do
   @page_title = 'New memo'
-  erb :new, layout: :layout
+  erb :new
 end
 
 get '/memos/:id' do
+  load
   @id = params['id']
   if @memo[@id] && !@memo[@id]['deleted_id']
     @page_title = 'Show memo'
-    erb :show, layout: :layout
+    erb :show
   else
     not_found
   end
 end
 
 get '/memos/:id/edit' do
+  load
   @id = params['id']
   if @memo[@id] && !@memo[@id]['deleted_id']
     @page_title = 'Edit memo'
-    erb :edit, layout: :layout
+    erb :edit
   else
     not_found
   end
 end
 
 post '/memos' do
-  id = @memo.size
-  @memo[id] = memo_params
+  load
+  id = (@memo.keys.map(&:to_i).max + 1)
+  @memo[id] = { 'memo_title' => params['memo_title'], 'memo_text' => params['memo_text'] }
   save(@memo)
 
   redirect '/memos', 303
 end
 
 patch '/memos/:id' do
+  load
   id = params['id']
-  @memo[id] = memo_params
+  @memo[id] = { 'memo_title' => params['memo_title'], 'memo_text' => params['memo_text'] }
   save(@memo)
 
   redirect "/memos/#{id}", 303
 end
 
 delete '/memos/:id' do
-  id = params['id']
-  @memo[id] = memo_params(id)
+  load
+  @memo.delete(params['id'])
   save(@memo)
 
   redirect '/memos', 303
 end
 
 not_found do
-  erb :error, layout: :layout
+  erb :error
 end
 
-def memo_params(id = nil)
-  if id
-    memo_title = @memo[id]['memo_title']
-    memo_text = @memo[id]['memo_text']
-  else
-    memo_title = params['memo_title']
-    memo_text = params['memo_text']
-  end
-  return { 'memo_title' => memo_title, 'memo_text' => memo_text, 'deleted_id' => 1 } if id
+MEMOS_FILE = 'public/memos.json'
 
-  { 'memo_title' => escape(memo_title), 'memo_text' => escape(memo_text), 'deleted_id' => nil }
-end
-
-helpers do
-  def escape(input)
-    escape_html(input)
-  end
+def load
+  @memo = JSON.parse(File.read(MEMOS_FILE))
 end
 
 def save(memo)
-  memo_json = JSON.pretty_generate(memo)
   File.open(MEMOS_FILE, 'w+') do |file|
-    file.write(memo_json)
+    file.write(memo.to_json)
   end
 end
